c908c1627bd3b22694a85b5f3326a053d6b699ff
Alla Petlovanna <103685111+alla-petlovanna@users.noreply.github.com>
2022-04-15 10:23:35 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoOverlay = document.querySelector('#video-story-overlay');
+const videoFile = document.querySelector('#video-story');
+
+videoBtn.addEventListener('click', function () {
+
+	function toggleOverlay(event){
+		if (event.type === 'mouseleave') {
+			videoOverlay.classList.add('hidden');
+		} else {
+			videoOverlay.classList.remove('hidden');
+		}
+	}
+
+	if (videoFile.paused) {
+		videoFile.play();
+		videoBtnIcon.src = './img/pause-white.svg';
+
+		videoOverlay.onmouseleave = toggleOverlay;
+		videoOverlay.onmouseenter = toggleOverlay;
+
+	} else {
+		videoFile.pause();
+		videoBtnIcon.src = './img/play-white.svg';
+		videoOverlay.onmouseleave = null;
+		videoOverlay.onmouseenter = null;
+	}
+
+})
